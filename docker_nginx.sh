ac6962fff1ff34e16aca5a2625a8ac9b0f743984
Alexander <128065248+filatov0120@users.noreply.github.com>
2023-08-23 23:14:21 +0300

    Create docker_nginx.sh

---
+++ docker_nginx.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#Install docker
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo usermod -aG docker ubuntu
+#Install NGINX
+sudo apt install -y curl gnupg2 ca-certificates lsb-release ubuntu-keyring
+sudo apt install -y nginx
+sudo apt-get upgrade -y
+sudo shutdown -r now
